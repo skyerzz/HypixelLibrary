f785b3e154f509d7cf2e709d38865785bb9ef47f
skyerzz <spammen67@gmail.com>
2016-12-26 18:01:02 +0100

    added a way of disabling info, warn and error logs individually.

--- src/com/skyerzz/hypixellib/Logger.java
+++ src/com/skyerzz/hypixellib/Logger.java
@@ -10,19 +10,39 @@ public class Logger {
 
     private static final String prefix = "[HypixelAPI] ";
 
+    private static boolean logInfo = true, logWarn = true, logError = true;
+
     public Logger(){
 
     }
 
     public static void logInfo(String s) {
-        System.out.println(prefix + "[INFO] " + s);
+        if(logInfo){
+            System.out.println(prefix + "[INFO] " + s);
+        }
     }
 
     public static void logWarn(String s) {
-        System.out.println(prefix + "[WARNING] " + s);
+        if(logWarn) {
+            System.out.println(prefix + "[WARNING] " + s);
+        }
     }
 
     public static void logError(String s) {
-        System.out.println(prefix + "[ERROR] " + s);
+        if(logError) {
+            System.out.println(prefix + "[ERROR] " + s);
+        }
+    }
+
+    public static void toggleErrorLogging(boolean errors){
+        logError = errors;
+    }
+
+    public static void toggleWarnLogging(boolean warn){
+        logWarn = warn;
+    }
+
+    public static void toggleInfoLogging(boolean info){
+        logInfo = info;
     }
 }
